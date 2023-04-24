7034b529aa27abd4f488c7b7dfc69721397da36f
Volodymyr Lysenko <lusiy321@gmail.com>
2023-04-24 21:12:45 +0300

    suum

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TelegramApi = require("node-telegram-bot-api");
+const order = require("./orderbook");
 const token = "5735264932:AAEtBZZeNqv6jB5zMFRaZSfquyX72dLRIsc";
 
 const bot = new TelegramApi(token, { polling: true });
@@ -6,29 +7,26 @@ const bot = new TelegramApi(token, { polling: true });
 bot.setMyCommands([
   { command: "/start", description: "Старт" },
   { command: "/info", description: "Информация о товаре" },
+  { command: "/update", description: "Информация" },
 ]);
 
 const start = () => {
   bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
-    const url = './img/blue-auto-mazar.jpg';
 
     if (text === "/start") {
-       bot.sendPhoto(chatId, url);
-       bot.sendMessage(
-        chatId,
-        `Привет ${msg.from.first_name}, для просмотра товара и оформление заказа перейди в меню`
-      );
-      
-      return
-    }
+      bot.sendMessage(chatId, `Привет ${msg.from.first_name}`);
 
+      return;
+    }
+    if (text === "/update") {
+      order;
+      bot.sendMessage(chatId, "Socket start");
+      return;
+    }
     if (text === "/info") {
-      return bot.sendMessage(
-        chatId,
-        `This is work`
-      );
+      return bot.sendMessage(chatId, `This is work`);
     }
 
     return bot.sendMessage(chatId, "Mesagge me");
