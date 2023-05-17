26be2fc06ba074f8b549bbe69e2db6774de18aac
VOLODYMYR LYSENKO <107991551+Lusiy321@users.noreply.github.com>
2023-05-17 16:27:33 +0300

    graf

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const TelegramApi = require("node-telegram-bot-api");
 require("dotenv").config();
 const token = process.env.TELEGRAM_TOKEN;
+const axios = require("axios");
+const { restart } = require("nodemon");
 
 const bot = new TelegramApi(token, { polling: true });
 
-bot.setMyCommands([
-  { command: "/start", description: "Старт" },
-  { command: "/info", description: "Информация о товаре" },
-  { command: "/update", description: "Информация" },
-]);
+bot.setMyCommands([{ command: "/start", description: "Старт" }]);
+
+async function checkPriceChange(chatId) {
+  try {
+    // Получение списка монет
+    const responseSymbols = await axios.get(
+      "https://api.bybit.com/spot/v3/public/symbols"
+    );
+    if (responseSymbols.status === 200) {
+      const quotes = responseSymbols.data.result.list;
+      for (const symbol of quotes) {
+        try {
+          const intervalResponse = await axios.get(
+            `https://api.bybit.com/spot/v3/public/quote/kline?symbol=${symbol.name}&interval=1m&limit=1`
+          );
+          if (intervalResponse.status === 200) {
+            const prices = intervalResponse.data.result.list;
+
+            for (const price of prices) {
+              const priceChangePercent = ((price.c - price.o) / price.o) * 100;
+
+              if (priceChangePercent >= 5 || priceChangePercent <= -5) {
+                bot.sendMessage(
+                  chatId,
+                  `Пара: #${
+                    symbol.name
+                  }, Цена изменилась на: ${priceChangePercent.toFixed(
+                    2
+                  )}% за 1мин. Объем: ${price.v}$`
+                );
+                console.log(
+                  `Symbol: ${
+                    symbol.name
+                  }, Price Change: ${priceChangePercent.toFixed(2)} %`
+                );
+              }
+            }
+          }
+        } catch (error) {
+          console.error(`For of: ${error.message}`);
+          continue;
+        }
+      }
+    }
+  } catch (error) {
+    console.error(`${error.message}`);
+  }
+}
 
 const start = () => {
   bot.on("message", async (msg) => {
@@ -16,7 +61,13 @@ const start = () => {
     const chatId = msg.chat.id;
 
     if (text === "/start") {
-      return bot.sendMessage(chatId, `Привет ${msg.from.first_name}`);
+      bot.sendMessage(
+        chatId,
+        `Привет ${msg.from.first_name} теперь тебе будут приходить уведомления об изменении цены криптовалют биржи Bybit на 5%`
+      );
+      console.log(`New user ${msg.from.first_name} connected`);
+      checkPriceChange(chatId);
+      return;
     }
     return bot.sendMessage(chatId, "Mesagge me");
   });
--- ws.js
+++ ws.js
@@ -1,18 +1,44 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const ws = new WebSocket('wss://stream.bybit.com/realtime');
+const ws = new WebSocket("wss://stream.bybit.com/realtime");
 
-const symbols = ["BTCUSDT", "ETHUSDT", "XRPUSDT", "ADAUSDT", "DOGEUSDT", "SOLUSDT", "DOTUSDT", "LINKUSDT", "LUNAUSDT", "AXSUSDT"];
+const symbols = [
+  "BTCUSDT",
+  "ETHUSDT",
+  "XRPUSDT",
+  "ADAUSDT",
+  "DOGEUSDT",
+  "SOLUSDT",
+  "DOTUSDT",
+  "LINKUSDT",
+  "LUNAUSDT",
+  "AXSUSDT",
+];
 
-ws.on('open', function open() {
+ws.on("open", function open() {
   for (const symbol of symbols) {
-    ws.send(JSON.stringify({"op": "subscribe", "args": [`instrument_info.100ms.${symbol}`]}));
+    ws.send(
+      JSON.stringify({
+        op: "subscribe",
+        args: [`instrument_info.100ms.${symbol}`],
+      })
+    );
   }
 });
 
-ws.on('message', function incoming(data) {
+ws.on("message", function incoming(data) {
   const message = JSON.parse(data);
-  if (message.success === true && message.topic && message.topic.includes('instrument_info.100ms.')) {
-    console.log(`Symbol: ${message.topic.split('.')[2]}, Last Price: ${message.data.last_price}`);
+  if (
+    message.success === true &&
+    message.topic &&
+    message.topic.includes("instrument_info.100ms.")
+  ) {
+    console.log(
+      `Symbol: ${message.topic.split(".")[2]}, Last Price: ${
+        message.data.last_price
+      }`
+    );
   }
 });
+
+module.exports = { ws };
